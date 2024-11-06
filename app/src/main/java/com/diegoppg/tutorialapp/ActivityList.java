9d3c842efb807f76e8449930799888e5a7064441
diegommppg <diegommppg@gmail.com>
2024-11-06 09:53:43 +0100

    ListView created

---
+++ app/src/main/java/com/diegoppg/tutorialapp/ActivityList.java
@@ -0,0 +1,50 @@
+package com.diegoppg.tutorialapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+
+        //Recuperar elementos
+        ListView listView=(ListView)findViewById(R.id.listView);
+
+        String[] listItem = getResources().getStringArray(R.array.paises);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                R.layout.item_list, R.id.textViewItem, listItem);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // TODO Auto-generated method stub
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/diegoppg/tutorialapp/FragmentsNavigation.java
+++ app/src/main/java/com/diegoppg/tutorialapp/FragmentsNavigation.java
@@ -1,8 +1,10 @@
 package com.diegoppg.tutorialapp;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -11,7 +13,14 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FragmentsNavigation extends AppCompatActivity {
 
@@ -42,13 +51,51 @@ public class FragmentsNavigation extends AppCompatActivity {
             NavigationUI.setupWithNavController(bottomNavigationView, navController);
         }
 
+        //ADD DATA
+        addData();
+
+        //READ DATA
 
 
 
+    } // FIN ON CREATE
 
+
+    //Metodo para añadir informacion a la base de datos
+    public void addData(){
+        String TAG = "Firebase";
+
+        //Recuperamos instancia de la base de datos
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //Añadir informacion a la base de datos
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
 
+    //Metodo para leer informacion a la base de datos
+    public void readData(){
+
+    }
 
 
 
